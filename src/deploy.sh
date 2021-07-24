627cfe83c749ba68ce374ea64523b1a638137759
lsw <skek7811@gmail.com>
2021-07-25 01:45:51 +0900

    ci 설정 추가

---
+++ src/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/front-end/app
+
+echo "> sudo npm install 실행"
+
+sudo npm install
+
+echo "> npm start 애플리케이션 실행"
+
+sudo npm start
---
+++ src/rm-old.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/front-end/
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fa react | grep front-end | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동 중인 애플리케이션이 존재하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> app 폴더 삭제"
+
+sudo rm -rdf app
+
+echo "> 새로운 app 폴더 생성"
+
+sudo mkdir app
